5855422fdb676499da59d0043b24d5ce4d661015
Francesco Infante <francesco@linkurio.us>
2020-12-30 17:14:39 +0100

    lint

--- src/backend/routeHandler.ts
+++ src/backend/routeHandler.ts
@@ -1,42 +1,49 @@
-import {PluginConfig, PluginRouteOptions} from '../@types/plugin';
 import bodyParser from 'body-parser';
 
-export = function configureRoutes(options: PluginRouteOptions<PluginConfig>): void {
+import {PluginConfig, PluginRouteOptions} from '../@types/plugin';
 
-  options.router.use( bodyParser.json() );
-  options.router.get( '/getOgmaConfiguration', async (req, res) => {
+export = function configureRoutes(options: PluginRouteOptions<PluginConfig>): void {
+  options.router.use(bodyParser.json());
+  options.router.get('/getOgmaConfiguration', async (req, res) => {
     try {
-      const LKEConfiguration = await options.getRestClient( req ).config.getConfiguration();
-      res.contentType( 'application/json' );
+      const LKEConfiguration = await options.getRestClient(req).config.getConfiguration();
+      res.contentType('application/json');
       if (LKEConfiguration.isSuccess()) {
-        res.status( 200 );
-        res.send( JSON.stringify( LKEConfiguration.body.ogma ) );
+        res.status(200);
+        res.send(JSON.stringify(LKEConfiguration.body.ogma));
       } else {
-        res.status( 412 );
-        res.send( JSON.stringify( {status: 412, body: {message: LKEConfiguration.body.message}} ) );
+        res.status(412);
+        res.send(JSON.stringify({status: 412, body: {message: LKEConfiguration.body.message}}));
       }
     } catch (e) {
-      res.status( 412 );
-      res.send( JSON.stringify( {status: 412, body: e} ) );
+      res.status(412);
+      res.send(JSON.stringify({status: 412, body: e}));
     }
-  } );
-  options.router.get( '/getVisualizationConfiguration/id=:id&sourceKey=:sourceKey', async (req, res) => {
-    try {
-      const visualizationConfiguration = await options.getRestClient( req ).visualization.getVisualization( {
-        sourceKey: req.params.sourceKey,
-        id: +req.params.id
-      } );
-      res.contentType( 'application/json' );
-      if (visualizationConfiguration.isSuccess()) {
-        res.status( 200 );
-        res.send( JSON.stringify( visualizationConfiguration.body ) );
-      } else {
-        res.status( 412 );
-        res.send( JSON.stringify( {status: 412, body: {message: visualizationConfiguration.body.message}} ) );
+  });
+  options.router.get(
+      '/getVisualizationConfiguration/id=:id&sourceKey=:sourceKey',
+      async (req, res) => {
+        try {
+          const visualizationConfiguration = await options
+              .getRestClient(req)
+              .visualization.getVisualization({
+                sourceKey: req.params.sourceKey,
+                id: +req.params.id
+              });
+          res.contentType('application/json');
+          if (visualizationConfiguration.isSuccess()) {
+            res.status(200);
+            res.send(JSON.stringify(visualizationConfiguration.body));
+          } else {
+            res.status(412);
+            res.send(
+                JSON.stringify({status: 412, body: {message: visualizationConfiguration.body.message}})
+            );
+          }
+        } catch (e) {
+          res.status(412);
+          res.send(JSON.stringify({status: 412, body: e}));
+        }
       }
-    } catch (e) {
-      res.status( 412 );
-      res.send( JSON.stringify( {status: 412, body: e} ) );
-    }
-  } );
+  );
 };
