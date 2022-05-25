30259303127ae50f8035cbe6e68a2fd6f925201d
Alexander Milevski <alexander@linkurio.us>
2022-05-25 17:50:59 +0200

    Bootstrapped panel

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,10 +1,19 @@
 import React from "react";
-
+import { Layout, Button } from "antd";
+import "antd/dist/antd.css";
 import { useAppContext } from "../context";
 import { Visualisation } from "./Visualisation";
+import { Panel } from "./Panel";
 
 export function Dashboard() {
   const { loading } = useAppContext();
   if (loading) return <div>Loading...</div>;
-  return <Visualisation />;
+  return (
+    <Layout>
+      <Layout.Content>
+        <Visualisation />
+      </Layout.Content>
+      <Panel />
+    </Layout>
+  );
 }
