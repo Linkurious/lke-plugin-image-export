e30e2e855305fe13bd17007d720ee3d0dbf1ab9c
Alexander Milevski <alexander@linkurio.us>
2022-05-25 17:55:27 +0200

    Bootstrapped panel

---
+++ src/components/Panel.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import { Modal, Button } from "antd";
+
+export function Panel() {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
+  return (
+    <div className="panel">
+      <Button type="primary" onClick={showModal}>
+        Export me please!
+      </Button>
+      <Modal
+        title="Basic Modal"
+        visible={isModalVisible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Modal>
+    </div>
+  );
+}
