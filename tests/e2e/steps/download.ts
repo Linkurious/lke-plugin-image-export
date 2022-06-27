aefb997206c23ff54be6e1399a994db4db2c2a02
Alexander Milevski <alexander@linkurio.us>
2022-06-27 15:24:58 +0200

    Increased timeouts

--- tests/e2e/steps/download.ts
+++ tests/e2e/steps/download.ts
@@ -1,35 +1,38 @@
-import Ogma, {
-} from '@linkurious/ogma';
-import fs from 'fs/promises';
-import mkdir from 'mkdirp';
-import path from 'path';
-import BlinkDiff from 'blink-diff';
-import { BrowserContext, Page } from 'playwright';
-import sharp  from "sharp"
+import Ogma from "@linkurious/ogma";
+import fs from "fs/promises";
+import mkdir from "mkdirp";
+import path from "path";
+import BlinkDiff from "blink-diff";
+import { BrowserContext, Page } from "playwright";
+import sharp from "sharp";
 
 const { I } = inject();
 const ogma = {} as Ogma;
 
-const rootFolder = '../../reports/html/e2e/';
-const screenshotFolder = path.join(rootFolder, 'screenshot/');
-const diffFolder = path.join(rootFolder, 'diff/');
-const baseFolder = './ref-images';
+const rootFolder = "../../reports/html/e2e/";
+const screenshotFolder = path.join(rootFolder, "screenshot/");
+const diffFolder = path.join(rootFolder, "diff/");
+const baseFolder = "./ref-images";
 let foldersCreated = false;
 const results: any[] = [];
 
-const shouldReplace = process.env.REPLACE === 'true' ? true: false;
-function getPaths(fileName: string){
+const shouldReplace = process.env.REPLACE === "true" ? true : false;
+function getPaths(fileName: string) {
   fileName = fileName.trim();
   return {
     diffPath: path.join(diffFolder, fileName),
     expectedPath: path.join(baseFolder, fileName),
-    actualPath: path.join(screenshotFolder, fileName), 
-  }
+    actualPath: path.join(screenshotFolder, fileName),
+  };
 }
 
-function compareImages(expectedPath: string, actualPath: string, diffPath: string ){
+function compareImages(
+  expectedPath: string,
+  actualPath: string,
+  diffPath: string
+) {
   const diff = new BlinkDiff({
-    imageAPath:expectedPath,
+    imageAPath: expectedPath,
     imageBPath: actualPath,
     thresholdType: BlinkDiff.THRESHOLD_PERCENT,
     threshold: 0.01, // 1% threshold
@@ -41,8 +44,8 @@ function compareImages(expectedPath: string, actualPath: string, diffPath: strin
       if (error) {
         reject(error);
       } else {
-        diff.hasPassed(result.code) 
-          ? resolve(result.differences) 
+        diff.hasPassed(result.code)
+          ? resolve(result.differences)
           : reject(result.differences);
       }
     });
@@ -50,93 +53,88 @@ function compareImages(expectedPath: string, actualPath: string, diffPath: strin
 }
 
 When(/^I drag the viz (\w+)/, async (shouldDrag) => {
-    if(shouldDrag === 'false') return;
-    await I.executeScript(() => {
-      ogma.setOptions({interactions: {drag: {enabled: false}}});
-    })
-    await I.usePlaywrightTo(
-      'drag the view',
-      async ({ page }: { page: Page; context: BrowserContext }) => {
-        const view = page.viewportSize();
-        if (!view) throw new Error('Could not get viewport size');
-        const cx = view.width / 2;
-        const cy = view.height / 2;
-        await page.mouse.move(cx, cy);
-        await page.mouse.down();
-        await page.waitForTimeout(200);
-        await page.mouse.move(cx + 300, cy + 300, { steps: 60 });
-        await page.waitForTimeout(200);
-        await page.mouse.up();
-      }
-    );
-    await I.executeScript(() => {
-      ogma.setOptions({interactions: {drag: {enabled: true}}});
-    })
-})
+  if (shouldDrag === "false") return;
+  await I.executeScript(() => {
+    ogma.setOptions({ interactions: { drag: { enabled: false } } });
+  });
+  await I.usePlaywrightTo(
+    "drag the view",
+    async ({ page }: { page: Page; context: BrowserContext }) => {
+      const view = page.viewportSize();
+      if (!view) throw new Error("Could not get viewport size");
+      const cx = view.width / 2;
+      const cy = view.height / 2;
+      await page.mouse.move(cx, cy);
+      await page.mouse.down();
+      await page.waitForTimeout(200);
+      await page.mouse.move(cx + 300, cy + 300, { steps: 60 });
+      await page.waitForTimeout(200);
+      await page.mouse.up();
+    }
+  );
+  await I.executeScript(() => {
+    ogma.setOptions({ interactions: { drag: { enabled: true } } });
+  });
+});
 
 When(/^I set text visibility (\w+)$/, async (shouldShow) => {
-  if(shouldShow === 'true') return;
-  I.click('.caption-switch');
-})
+  if (shouldShow === "true") return;
+  I.click(".caption-switch");
+});
 
 When(/^I set text collision removal (\w+)$/, async (shouldRemoveCollision) => {
-  if(shouldRemoveCollision === 'true') return;
-  I.click('.collision-switch');
-})
+  if (shouldRemoveCollision === "true") return;
+  I.click(".collision-switch");
+});
 
 When(/^I select output format (.+)$/, async (format) => {
-  if(format ==='svg')return;
-  await I.click('SVG');
-  await I.click('.ant-dropdown-menu-title-content');
-})
+  if (format === "svg") return;
+  await I.click("SVG");
+  await I.click(".ant-dropdown-menu-title-content");
+});
 
 When(/^I click download (.+) (.+)$/, async (name, format) => {
-  const [download, downloadPath] = await I.download('text=Download');
-  const {expectedPath, actualPath} = getPaths(name);
-  if(!downloadPath){
-    throw('download failed');
+  const [download, downloadPath] = await I.download("text=Download");
+  const { expectedPath, actualPath } = getPaths(name);
+  if (!downloadPath) {
+    throw "download failed";
   }
   const outPath = shouldReplace ? expectedPath : actualPath;
-  if(format === 'svg'){
-    return await (sharp(downloadPath)
-    .png()
-    .toFile(outPath))
+  if (format === "svg") {
+    return await sharp(downloadPath).png().toFile(outPath);
   }
   return await fs.copyFile(downloadPath, outPath);
 });
 
 Then(/^image is nice (.+)$/, (name) => {
-  const {expectedPath, actualPath, diffPath} = getPaths(name);
-  if(shouldReplace){
+  const { expectedPath, actualPath, diffPath } = getPaths(name);
+  if (shouldReplace) {
     return;
-  }else{
-    return compareImages(
-      actualPath,
-      expectedPath,
-      diffPath,
-      )
-    .then(() => {
-      results.push({name, path: diffPath, success: true});
-    })
-    .catch(e => {
-      results.push({name, path: diffPath, success: false});
-      throw(e);
-    });
+  } else {
+    return compareImages(actualPath, expectedPath, diffPath)
+      .then(() => {
+        results.push({ name, path: diffPath, success: true });
+      })
+      .catch((e) => {
+        results.push({ name, path: diffPath, success: false });
+        throw e;
+      });
   }
-})
+});
 
 //@ts-ignore
 Before(async ({ tags }) => {
-  if (!tags.includes('@download') || foldersCreated) return;
+  if (!tags.includes("@download") || foldersCreated) return;
   await mkdir(path.resolve(screenshotFolder));
   await mkdir(path.resolve(diffFolder));
   await mkdir(path.resolve(baseFolder));
   foldersCreated = true;
 });
+
 //@ts-ignore
-After(({tags}) => {
+After(({ tags }) => {
   return fs.writeFile(
-    path.join(rootFolder, 'export-results.json'),
+    path.join(rootFolder, "export-results.json"),
     JSON.stringify(results)
   );
 });
