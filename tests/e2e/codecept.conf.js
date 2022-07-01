6064495bded46e1b8d9220a2934abb03086d338a
Alexander Milevski <alexander@linkurio.us>
2022-07-01 10:59:12 +0200

    Added compression

--- tests/e2e/codecept.conf.js
+++ tests/e2e/codecept.conf.js
@@ -1,6 +1,6 @@
-require('ts-node/register');
+require("ts-node/register");
 
-const { setHeadlessWhen, setWindowSize } = require('@codeceptjs/configure');
+const { setHeadlessWhen, setWindowSize } = require("@codeceptjs/configure");
 
 setHeadlessWhen(process.env.CI);
 
@@ -15,41 +15,41 @@ const emulateOptions = process.env.TEST_RETINA
   : { isMobile: false, deviceScaleFactor: 1 };
 
 exports.config = {
-  output: '../../reports/html/e2e',
+  output: "../../reports/html/e2e",
   timeout: 25,
   helpers: {
     Playwright: {
       url: baseUrl,
       show: true,
-      browser: 'chromium',
+      browser: "chromium",
       emulate: emulateOptions,
       chromium: {
         // slowMo: 800,
         // chromiumSandbox: true, // https://playwright.dev/docs/ci/#docker
-        args: []
-      }
+        args: ["--headless"],
+      },
     },
     CustomHelper: {
-      require: './helpers/customHelper'
-    }
+      require: "./helpers/customHelper",
+    },
   },
   gherkin: {
-    features: './features/**/*.feature',
-    steps: './steps/**/*.ts'
+    features: "./features/**/*.feature",
+    steps: "./steps/**/*.ts",
   },
-  name: 'lkeplugin-image-export',
+  name: "lkeplugin-image-export",
   grep: process.env.GREP,
   plugins: {
     testomat: {
       enabled: true,
-      require: '@testomatio/reporter/lib/adapter/codecept',
-      apiKey: process.env.TESTOMATIO
+      require: "@testomatio/reporter/lib/adapter/codecept",
+      apiKey: process.env.TESTOMATIO,
     },
     stepByStepReport: {
       enabled: process.env.CI,
-      output: '../../reports/html/e2e',
-      ignoreSteps: ['grab*', 'wait*'],
-      fullPageScreenshots: true
-    }
+      output: "../../reports/html/e2e",
+      ignoreSteps: ["grab*", "wait*"],
+      fullPageScreenshots: true,
+    },
   },
 };
