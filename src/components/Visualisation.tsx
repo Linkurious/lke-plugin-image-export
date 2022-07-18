3d80aae7afe4b6601d8f8353dd965725e84d4e8f
Alexander Milevski <alexander@linkurio.us>
2022-07-18 16:43:43 +0200

    Added version info and docs link in the bottom right corner

--- src/components/Visualisation.tsx
+++ src/components/Visualisation.tsx
@@ -4,6 +4,7 @@ import { Backdrop } from "./Backdrop";
 import { Minimap } from "./Minimap";
 import { Ogma } from "./Ogma";
 import { ZoomControl } from "./ZoomControl";
+import { VersionInfo } from "./VersionInfo";
 
 export function Visualisation() {
   const { visualisation, configuration, setOgma, format } = useAppContext();
@@ -16,6 +17,7 @@ export function Visualisation() {
       >
         <ZoomControl />
         <Minimap />
+        <VersionInfo />
       </Ogma>
       <Backdrop format={format} />
     </>
