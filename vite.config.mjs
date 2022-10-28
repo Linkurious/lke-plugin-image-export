a00c240e1c48e5fd41a0cf9086e9b87e73e976e1
Alexander Milevski <alexander@linkurio.us>
2022-10-28 17:15:07 +0200

    Added legacy plugin to support Edge 16

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,10 +1,16 @@
 import path from "path";
+import legacy from "@vitejs/plugin-legacy";
 
 /** @type {import('vite').UserConfig} */
 export default {
   base: "",
   root: "web",
-  plugins: [],
+  plugins: [
+    legacy({
+      targets: ["defaults", "not IE 11"],
+    }),
+  ],
+
   server: {
     port: 4001,
     open: true,
@@ -13,7 +19,11 @@ export default {
     IS_DEV: process.env.DEV ? true : false,
   },
   build: {
+    manifest: true,
     outDir: path.join(process.cwd(), "dist", "public"),
+    rollupOptions: {
+      format: "iife",
+    },
     emptyOutDir: true,
   },
   proxy: {
