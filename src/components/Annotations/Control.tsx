0e023c01b9927cc769266d1a29ab61fb8ed393d1
Alexander Milevski <alexander@linkurio.us>
2022-12-20 23:36:49 +0100

    Dropped in all the UI

--- src/components/Annotations/Control.tsx
+++ src/components/Annotations/Control.tsx
@@ -1,10 +1,25 @@
 import React, { FC, useEffect, useLayoutEffect, useState, useRef } from "react";
-import { Dropdown, Menu, Button, Divider, Space } from "antd";
+import { Dropdown, Menu, MenuProps, Button, Divider, Space, Radio } from "antd";
 import { SubnodeOutlined, DownOutlined } from "@ant-design/icons";
 import { stopPropagation } from "../../utils";
 import { Control } from "@linkurious/text-annotations";
-import { Text as TextIcon, MapsArrowDiagonal, ArrowRight } from "iconoir-react";
+import {
+  Text as TextIcon,
+  MapsArrowDiagonal,
+  ArrowRight,
+  ArrowLeft,
+  NavArrowLeft,
+  NavArrowRight,
+  Minus,
+  MoreHoriz,
+  MoveRight,
+  MoveLeft,
+  Circle,
+  Cancel,
+} from "iconoir-react";
+import { TwitterPicker as ColorPicker } from "react-color";
 import { useAppContext } from "../../context";
+import { DoubleArrowIcon } from "./DoubleArrowIcon";
 
 import "@linkurious/text-annotations/style.css";
 import "./Control.css";
@@ -28,29 +43,80 @@ export const AnnotationsControl: FC<Props> = ({}) => {
     }
   }, [ogma]);
 
+  const fontItems: MenuProps["items"] = [
+    { key: "Normal", label: "Roboto" },
+    { key: "Serif", label: "Serif" },
+    { key: "Monospace", label: "Monospace" },
+  ];
+
+  const fontSizeItems: MenuProps["items"] = [
+    { key: "8", label: "8" },
+    { key: "10", label: "10" },
+    { key: "12", label: "12" },
+    { key: "14", label: "14" },
+    { key: "16", label: "16" },
+  ];
+
+  const lineWidthItems: MenuProps["items"] = [
+    { key: "1", label: "1" },
+    { key: "2", label: "2" },
+    { key: "3", label: "3" },
+    { key: "4", label: "4" },
+  ];
+
   return (
     <div className="annotations-control" ref={containerRef}>
       <Button.Group>
         <Dropdown.Button
-          icon={<DownOutlined />}
+          size="small"
+          className="annotations-text--dropdown"
+          icon={<DownOutlined height={1.5} width={5} />}
           menu={{ items: [] }}
           onClick={() => console.log("add text")}
           trigger={["click"]}
           dropdownRender={(menu) => (
             <div className="annotations-control--panel dropdown-content">
-              <Button type="primary">Click me!</Button>
+              <Dropdown.Button
+                menu={{ items: fontItems }}
+                trigger={["click"]}
+                icon={<DownOutlined />}
+              >
+                Sans-serif
+              </Dropdown.Button>
+              <Dropdown.Button
+                menu={{ items: fontSizeItems }}
+                trigger={["click"]}
+                icon={<DownOutlined />}
+              >
+                12
+              </Dropdown.Button>
+              <ColorPicker triangle="hide" />
             </div>
           )}
         >
           <TextIcon height={iconSize} width={iconSize} fr="" />
         </Dropdown.Button>
         <Dropdown.Button
+          size="small"
+          className="annotations-arrow--dropdown"
           trigger={["click"]}
           icon={<DownOutlined />}
           onClick={() => console.log("add arrow")}
           dropdownRender={(menu) => (
             <div className="annotations-control--panel dropdown-content">
-              <Button type="primary">Click me!</Button>
+              <Button.Group>
+                <Button value="large">
+                  <DoubleArrowIcon />
+                </Button>
+                <Button value="default">
+                  <Minus fr="" />
+                </Button>
+                <Button value="small">
+                  <MoveRight fr="" />
+                </Button>
+              </Button.Group>
+              <Menu items={lineWidthItems} />
+              <ColorPicker triangle="hide" />
             </div>
           )}
         >
---
+++ src/components/Annotations/DoubleArrowIcon.tsx
@@ -0,0 +1,21 @@
+import React, { FC } from "react";
+
+export const DoubleArrowIcon: FC = () => (
+  <svg
+    width="1.5em"
+    height="1.5em"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    color="#000000"
+  >
+    <path
+      d="M 9 12 h 10 m 4 0 l -3 -3 m 3 3 l -3 3 m -12 -6 l -3 3 m 0 0 l 3 3"
+      transform="translate(-2,0)"
+      stroke="#000000"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+);
