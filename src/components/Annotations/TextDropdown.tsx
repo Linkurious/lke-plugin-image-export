b8f7c7b410ebbe0ad12554737a50734c619ae2c6
Alexander Milevski <alexander@linkurio.us>
2022-12-22 01:08:41 +0100

    Start adding texts

--- src/components/Annotations/TextDropdown.tsx
+++ src/components/Annotations/TextDropdown.tsx
@@ -1,20 +1,27 @@
-import React, { FC } from "react";
+import React, { FC, useCallback } from "react";
 import { Dropdown } from "antd";
 import { DownArrowIcon } from "./DownArrowIcon";
 import { Text as TextIcon } from "iconoir-react";
 import { TextStylePanel } from "./TextStylePanel";
 import { iconSize } from "./constants";
+import { useAnnotationsContext } from "../../context/annotations";
 
-export const TextDropdown: FC = () => (
-  <Dropdown.Button
-    size="small"
-    className="annotations-text--dropdown"
-    icon={<DownArrowIcon />}
-    menu={{ items: [] }}
-    onClick={() => console.log("add text")}
-    trigger={["click"]}
-    dropdownRender={() => <TextStylePanel />}
-  >
-    <TextIcon height={iconSize} width={iconSize} fr="" />
-  </Dropdown.Button>
-);
+export const TextDropdown: FC = () => {
+  const { editor, annotations, textStyle } = useAnnotationsContext();
+  const onClick = useCallback(() => {
+    console.log("add text");
+  }, [editor, annotations, textStyle]);
+  return (
+    <Dropdown.Button
+      size="small"
+      className="annotations-text--dropdown"
+      icon={<DownArrowIcon />}
+      menu={{ items: [] }}
+      onClick={onClick}
+      trigger={["click"]}
+      dropdownRender={() => <TextStylePanel />}
+    >
+      <TextIcon height={iconSize} width={iconSize} fr="" />
+    </Dropdown.Button>
+  );
+};
