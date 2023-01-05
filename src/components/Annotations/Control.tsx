82b561b082a515dde4c34b68e70a18ecec8075c5
Alexander Milevski <alexander@linkurio.us>
2023-01-05 16:13:53 +0100

    Read the style from selected

--- src/components/Annotations/Control.tsx
+++ src/components/Annotations/Control.tsx
@@ -1,7 +1,11 @@
-import React, { FC, useEffect, useRef } from "react";
+import React, { FC, useCallback, useEffect, useRef } from "react";
 import { Button } from "antd";
 import { stopPropagation } from "../../utils";
-import { Control as AnnotationsEditor } from "@linkurious/annotations-control";
+import {
+  Control as AnnotationsEditor,
+  isArrow,
+  isText,
+} from "@linkurious/annotations-control";
 
 import { useAppContext } from "../../context";
 import { useAnnotationsContext } from "../../context/annotations";
@@ -19,27 +23,46 @@ export const AnnotationsControl: FC = () => {
     editor,
     setEditor,
     arrowStyle,
+    setArrowStyle,
     textStyle,
+    setTextStyle,
     currentAnnotation,
     setCurrentAnnotation,
   } = useAnnotationsContext();
 
+  // stop propagation of mouse events to the ogma canvas
   useEffect(() => {
     if (containerRef.current) {
       containerRef.current.addEventListener("mousemove", stopPropagation);
     }
   }, [containerRef]);
 
+  // create the annotations editor when the ogma instance is available
   useEffect(() => {
     if (ogma) {
       const newEditor = new AnnotationsEditor(ogma);
       newEditor
-        .on("select", (annotation) => setCurrentAnnotation(annotation))
+        .on("select", (annotation) => {
+          // read back the current options from the selected annotation
+          if (isArrow(annotation)) {
+            setArrowStyle({
+              ...arrowStyle,
+              ...(annotation.properties.style || {}),
+            });
+          } else if (isText(annotation)) {
+            setTextStyle({
+              ...textStyle,
+              ...(annotation.properties.style || {}),
+            });
+          }
+          setCurrentAnnotation(annotation);
+        })
         .on("unselect", () => setCurrentAnnotation(null));
       setEditor(newEditor);
     }
   }, [ogma]);
 
+  // update the style of the current arrow annotation when the style changes
   useEffect(() => {
     console.log("arrow style changed", currentAnnotation);
     if (
@@ -51,7 +74,9 @@ export const AnnotationsControl: FC = () => {
     }
   }, [editor, arrowStyle]);
 
+  // update the style of the current text annotation when the style changes
   useEffect(() => {
+    console.log("text style changed", currentAnnotation);
     if (
       editor &&
       currentAnnotation &&
