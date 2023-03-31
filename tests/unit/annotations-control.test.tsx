fe27d8fc47da82264fd3ece6754ed538b84f36a3
Alexander Milevski <alexander@linkurio.us>
2023-03-31 10:01:45 +0200

    Made the test pass

--- tests/unit/annotations-control.test.tsx
+++ tests/unit/annotations-control.test.tsx
@@ -1,10 +1,21 @@
-import { describe, it } from "vitest";
+import { describe, expect, it } from "vitest";
 import { render, screen } from "@testing-library/react";
 import { AnnotationsControl } from "../../src/components/Annotations/Control";
 import React from "react";
+import {
+  AnnotationsContextProvider,
+  AppContextProvider,
+} from "../../src/context";
 
 describe("AnnotationsControl", () => {
   it("should render", () => {
-    render(<AnnotationsControl />);
+    render(
+      <AppContextProvider>
+        <AnnotationsContextProvider>
+          <AnnotationsControl />
+        </AnnotationsContextProvider>
+      </AppContextProvider>
+    );
+    expect(screen);
   });
 });
