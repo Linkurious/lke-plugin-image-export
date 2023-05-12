b0205aaaa6d919f7da837bf07b66dbe1350dc21b
Alexander Milevski <alexander@linkurio.us>
2023-05-13 00:18:23 +0200

    Build so that the fonts are packaged properly [skip:E2e tests]

--- web/assets/index.tsx
+++ web/assets/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOMClient from "react-dom/client";
-import App from "../src/App";
-import "./assets/css/styles.css";
+import App from "../../src/App";
+import "./css/styles.css";
 
 const container =
   document.getElementById("root") || document.querySelector("lk-app-root");
