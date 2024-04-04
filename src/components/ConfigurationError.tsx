52cb0ec27b8a51b742172ca7badc65127599ee4f
aminerhanemi <amine@linkurio.us>
2024-04-04 14:10:48 +0200

    revert change in error component

--- src/components/ConfigurationError.tsx
+++ src/components/ConfigurationError.tsx
@@ -6,13 +6,13 @@ interface ConfigurationErrorProps {
   error: Error;
 }
 
-export const ConfigurationError: FC<ConfigurationErrorProps> = ({error}) => (
+export const ConfigurationError: FC<ConfigurationErrorProps> = () => (
   <Layout>
     <Layout.Content>
       <Result
         status="error"
         title="Configuration error"
-        subTitle={error.message}
+        subTitle="Error while trying to read the configuration, please reload the page"
       />
     </Layout.Content>
   </Layout>
