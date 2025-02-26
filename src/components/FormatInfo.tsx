36274e38cf1cc1b939103fd9539f313766e1e7fa
Leo Nicolle <leo@linkurio.us>
2025-02-26 17:00:22 +0100

    fixed left panel size display

--- src/components/FormatInfo.tsx
+++ src/components/FormatInfo.tsx
@@ -1,12 +1,11 @@
-import React, { FC, useEffect, useState } from "react";
-import { fullSizeMargin } from "../constants";
+import { FC, useEffect, useState } from "react";
 import { useAnnotationsContext, useAppContext } from "../context";
 import { FormatType } from "../types/formats";
-import { formatSize, mergeBounds } from "../utils";
-import { getAnnotationsBounds } from "@linkurious/ogma-annotations";
+import { formatSize } from "../utils";
+import { defaultOptions, getExportSize } from "../utils/svg";
 
 export const FormatInfo: FC<FormatType> = ({ value }) => {
-  const { ogma, boundingBox, format, textsVisible } = useAppContext();
+  const { ogma, boundingBox, textsVisible } = useAppContext();
   const { annotations } = useAnnotationsContext();
   const [, setZoom] = useState(1);
   let dimensions = "";
@@ -27,19 +26,11 @@ export const FormatInfo: FC<FormatType> = ({ value }) => {
 
   if (!boundingBox || !ogma) return null;
 
-  const combinedBoundingBox =
-    annotations.features.length === 0
-      ? boundingBox
-      : mergeBounds(getAnnotationsBounds(annotations), boundingBox);
-
   if (typeof value === "undefined") {
-    const margin = format.value === undefined ? fullSizeMargin : 0;
-    const [minX, minY, maxX, maxY] = combinedBoundingBox;
+    const { width, height } = getExportSize(ogma, annotations, {
+      ...defaultOptions,
+    });
 
-    const lb = ogma.view.graphToScreenCoordinates({ x: minX, y: minY });
-    const rt = ogma.view.graphToScreenCoordinates({ x: maxX, y: maxY });
-    const width = Math.round((rt.x - lb.x) * (1 + margin * 2));
-    const height = Math.round((rt.y - lb.y) * (1 + margin * 2));
     dimensions = formatSize({ width, height });
   } else dimensions = formatSize(value);
 
